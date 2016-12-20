9c1442786db1b23acca4c998c1b03f215d75b3d3
Michael <michael.mebane@gmail.com>
2016-12-20 18:18:28 -0500

    Finishing up free and paid flavors.  Fixing the right files and finding out which bugs remain.

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -0,0 +1,33 @@
+package com.udacity.gradle.builditbigger.paid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.udacity.gradle.builditbigger.R;
+
+/**
+ * Created by jatin on 10/17/16.
+ */
+
+public class MainActivityFragment extends Fragment {
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+/*        Button button = (Button) root.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).tellJoke();
+            }
+        });*/
+
+        return root;
+    }
+}
